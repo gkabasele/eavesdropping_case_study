02ebcb5eb15f0536bc15fc4371844bb247d2ef28
gkabasele <Gnicolas.KN@gmail.com>
2016-12-22 09:56:33 +0100

    paths evaluation

--- bestPath.py
+++ bestPath.py
@@ -1,6 +1,7 @@
 # !/bin/python
 from bhandari import Graph,DisjointPath
 import copy
+import heapq
 
 BHAN = "bhandari"
 NA = "naive"
@@ -13,6 +14,7 @@ COMMON_VERTEX = 3
 class BestPath:
     def __init__(self):
         self.solutions = {}
+        self.priority_queue = []
 
     def get_naive_solution(self):
         if NA in self.solutions:
@@ -30,10 +32,16 @@ class BestPath:
 
     def n_shortest_path(self,src,dst,graph,n):
         disjoint = DisjointPath() 
+        #TODO what happens when not enough disjoint path
         (paths,shortest_path) = disjoint.disjoint_path_bhandari(src,dst,graph,n)
         self.solutions[BHAN] = paths
 
         other_paths = [shortest_path]
+        self.common_vertex_shortest_path(paths,shortest_path)
+        for i in range(n-1):
+            # priority queue contains tuple (#common_link,path)
+            other_paths.append(heapq.heappop(self.priority_queue)[1])
+        '''
         new_graph = copy.deepcopy(graph)
         self.remove_edge_along_path(shortest_path,new_graph)
         #TODO what happens when no disjoint path possible
@@ -42,19 +50,35 @@ class BestPath:
 
             if len(path) == 0:
                 #if no path found we add a vertex
-                i = len(path) - 1
-                while len(path) == 0 and i > 0:
-                    n = shortest_path(i)
-                    prev = shortest_path(i-1)
-                    previous_cost = graph.cost_edge(n,prev)
-                    new_graph.addVertex(n,prev)
+                i = 0
+                while len(path) == 0 and i < len(shortest_path)-1:
+                    n = shortest_path[i]
+                    succ = shortest_path[i+1]
+                    previous_cost = graph.cost_edge(n,succ)
+                    new_graph.addVertex(n,succ,previous_cost)
                     path = disjoint.get_path(src,dst,new_graph)
+                    i +=1
                     
             other_paths.append(path)
             new_graph = copy.deepcopy(new_graph)
             self.remove_edge_along_path(path,new_graph)
-                        
+        '''                
         self.solutions[NA] = other_paths
+
+    def common_vertex_shortest_path(self,paths,shortest_path):
+        vertices = set()
+        for i in range(len(shortest_path)-1):
+            vertices.add((shortest_path[i],shortest_path[i+1]))
+            vertices.add((shortest_path[i+1],shortest_path[i+1]))
+
+        for path in paths:
+            common_vertex = 0
+            for i in range(len(path)-1):
+                if (path[i],path[i+1]) in vertices:
+                    common_vertex +=1
+            heapq.heappush(self.priority_queue,(common_vertex,path))
+
+
         
     def count_common_vertex(self,paths):
         vertices = set()
@@ -74,10 +98,19 @@ class BestPath:
             cost+= graph.cost_edge(path[i],path[i+1])
         return cost
 
+    def longest_path_cost(self,paths,graph):
+        max_cost = 0
+        for i in range(len(paths)):
+            current_cost = self.count_path_cost(paths[i],graph)
+            if current_cost > max_cost:
+                max_cost = current_cost
+        return max_cost
+
+
     def objective_function(self,paths,graph):
         common = COMMON_VERTEX * self.count_common_vertex(paths)
-        nb_path = NB_PATH * len(paths)
-        cost = COST * self.count_path_cost(paths[0],graph)
-        return common + cost - nb_path
+        #nb_path = NB_PATH * len(paths)
+        cost = COST * self.longest_path_cost(paths,graph)
+        return common + cost 
 
 
--- test_bestPath.py
+++ test_bestPath.py
@@ -4,7 +4,7 @@ from bestPath import BestPath
 from bhandari import Graph
 
 if __name__=='__main__':
-
+    '''
     graph3 = Graph()
     graph3.addNode(1)
     graph3.addNode(2)
@@ -31,7 +31,7 @@ if __name__=='__main__':
 
 
     assert path_computer.objective_function(naive_path,graph3) == path_computer.objective_function(bhandari_path,graph3)
-    
+    ''' 
     graph = Graph()
     graph.addNode(1)
     graph.addNode(2)
@@ -53,9 +53,40 @@ if __name__=='__main__':
     graph.addVertex(6,7,1)
     graph.addVertex(7,8,1)
 
+    path_computer = BestPath()
+
     path_computer.n_shortest_path(1,8,graph,2)
 
     naive_path = path_computer.get_naive_solution()
     bhandari_path = path_computer.get_bhandari_solution()
 
-    assert path_computer.objective_function(naive_path,graph) < path_computer.objective_function(bhandari_path,graph) 
+    print path_computer.objective_function(naive_path,graph)
+    print path_computer.objective_function(bhandari_path,graph)
+
+    #assert path_computer.objective_function(naive_path,graph) < path_computer.objective_function(bhandari_path,graph) 
+    
+    graph2 = Graph()
+    graph2.addNode(1)
+    graph2.addNode(2)
+    graph2.addNode(3)
+    graph2.addNode(4)
+    graph2.addNode(5)
+    graph2.addNode(6)
+
+    graph2.addVertex(1,2,1)
+    graph2.addVertex(1,5,2)
+    graph2.addVertex(2,3,1)
+    graph2.addVertex(2,6,2)
+    graph2.addVertex(3,4,1)
+    graph2.addVertex(3,5,1)
+    graph2.addVertex(4,6,1)
+
+    path_computer = BestPath()
+    path_computer.n_shortest_path(1,4,graph2,2)
+
+    naive_path = path_computer.get_naive_solution()
+    bhandari_path = path_computer.get_bhandari_solution()
+
+    print path_computer.objective_function(naive_path,graph2)
+    print path_computer.objective_function(bhandari_path,graph2)
+    
